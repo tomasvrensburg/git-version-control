703ebabef4d5ae8f56b3ea2a211d9d45fd0fd35b
tomasvrensburg <tomasvrensburg@gmail.com>
2024-03-11 12:00:41 +0200

    Added routing to App.js

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,11 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
+import Landing from './components/Landing/Landing';
+import Products from './components/Products/Products';
+import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route path="/cart" element={<ShoppingCart />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
     </div>
   );
 }
