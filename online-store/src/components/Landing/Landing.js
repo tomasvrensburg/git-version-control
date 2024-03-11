cd961139f6d9850e1f9876efbedac6ae553569aa
tomasvrensburg <tomasvrensburg@gmail.com>
2024-03-11 13:26:43 +0200

    Added Header component to Landing.js

--- online-store/src/components/Landing/Landing.js
+++ online-store/src/components/Landing/Landing.js
@@ -1,5 +1,10 @@
+import Header from "../Header/Header";
+
 export default function Landing() {
   return (
-    <h1>This is the Landing Page</h1>
+    <div className="Landing">
+      <Header />
+      <h1>This is the Landing Page</h1>
+    </div>
   );
 }
