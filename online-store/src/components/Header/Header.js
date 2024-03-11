72fa1d022f92db2d4149b8ef63d91017b5c60d6a
tomasvrensburg <tomasvrensburg@gmail.com>
2024-03-11 11:19:27 +0200

    Added own content to navbar in Header.js

--- online-store/src/components/Header/Header.js
+++ online-store/src/components/Header/Header.js
@@ -1,19 +1,22 @@
-import Button from 'react-bootstrap/Button';
+// Importing Bootstrap components
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
+// Importing Logo
+import logo from '../../images/platoLogo.webp';
 
 export default function Header() {
   return (
     <header>
 
       {/* Off-canvas Navbar from Bootstrap */}
-      <Navbar expand={false} className="bg-body-tertiary mb-3">
+      <Navbar expand={false} className="mb-3">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+          <Navbar.Brand href="#">
+            <img src={logo} alt='"Plato Coffee" Logo' className='img-fluid' />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="offcanvasNavbar" />
           <Navbar.Offcanvas
             id="offcanvasNavbar"
@@ -21,12 +24,12 @@ export default function Header() {
             placement="end"
           >
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel">Offcanvas</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
               <Nav className="justify-content-end flex-grow-1 pe-3">
                 <Nav.Link href="#action1">Home</Nav.Link>
-                <Nav.Link href="#action2">Link</Nav.Link>
+                <Nav.Link href="#action2">Products</Nav.Link>
+                <Nav.Link href="#action2">Shopping Cart</Nav.Link>
                 <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
                   <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
@@ -34,15 +37,6 @@ export default function Header() {
                   <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
                 </NavDropdown>
               </Nav>
-              <Form className="d-flex">
-                <Form.Control
-                  type="search"
-                  placeholder="Search"
-                  className="me-2"
-                  aria-label="Search"
-                />
-                <Button variant="outline-success">Search</Button>
-              </Form>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
         </Container>
