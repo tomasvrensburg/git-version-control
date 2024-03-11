9972bea964391516d1bfa03e70911988fb3d98fc
tomasvrensburg <tomasvrensburg@gmail.com>
2024-03-11 14:14:03 +0200

    Added basic markup to Landing.js

--- online-store/src/components/Landing/Landing.js
+++ online-store/src/components/Landing/Landing.js
@@ -4,7 +4,12 @@ export default function Landing() {
   return (
     <div className="Landing">
       <Header />
-      <h1>This is the Landing Page</h1>
+      <div className="banner-image" />
+      <h6>WELL HELLŌ</h6>
+      <h2>Coffee sculpted. Community included.</h2>
+      <p>
+        Come experience the ultimate coffee experience at Plato Coffee. Our premium coffee are expertly crafted to deliver exceptional flavours and quality. From bold and rich to smooth and creamy, we have something to satisfy every palate. Our cosy atmosphere and friendly staff create the perfect environment to relax and enjoy your favourite brew. Visit us today and taste the difference.
+      </p>
     </div>
   );
 }
