8302571758bc8e17ede19783516cfebb173d1a14
tomasvrensburg <tomasvrensburg@gmail.com>
2024-03-11 11:05:42 +0200

    Added Header.js and imported component into App.js

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
     </div>
   );
 }
---
+++ online-store/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+export default function Header() {
+  return (
+    <header>
+
+    </header>
+  );
+}
