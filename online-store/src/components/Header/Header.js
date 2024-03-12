dca417bd1885f8612e2b90e3d310589ac46cb3a0
tomasvrensburg <tomasvrensburg@gmail.com>
2024-03-12 12:42:56 +0200

    Removed dropdown from navigation in Header.js

--- online-store/src/components/Header/Header.js
+++ online-store/src/components/Header/Header.js
@@ -49,12 +49,6 @@ export default function Header() {
                 <Nav.Link>
                   <Link to="/cart" className='navigation-link'>Shopping Cart</Link>
                 </Nav.Link>
-                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
-                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-                </NavDropdown>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
