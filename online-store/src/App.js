13e7accc001de74f7ca1bca306d75f66cf8c95ee
tomasvrensburg <tomasvrensburg@gmail.com>
2024-03-12 11:51:31 +0200

    Added Header component to App.js

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -5,10 +5,12 @@ import Landing from './components/Landing/Landing';
 import Products from './components/Products/Products';
 import ShoppingCart from './components/ShoppingCart/ShoppingCart';
 import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route exact path="/" element={<Landing />} />
         <Route path="/cart" element={<ShoppingCart />} />
