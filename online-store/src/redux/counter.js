59ae8f593138bee7b11c89bff3352c08a9d5916e
tomasvrensburg <tomasvrensburg@gmail.com>
2024-03-13 13:06:30 +0200

    IMported photos into counter.js

--- online-store/src/redux/counter.js
+++ online-store/src/redux/counter.js
@@ -1,56 +1,66 @@
 import { createSlice } from '@reduxjs/toolkit'
-
+// Importing image sources
+import productOne from '../images/IMG_0949.jpg.webp';
+import productTwo from '../images/IMG_0895.jpg.webp';
+import productThree from '../images/IMG_0874.jpg.webp';
+import productFour from '../images/IMG_0716.jpg.webp';
+import productFive from '../images/productFive.jpg';
+import productSix from '../images/productSix.jpg';
+import productSeven from '../images/productSeven.jpg';
+import productEight from '../images/productEight.jpg';
+import productNine from '../images/productNine.jpg';
+import productTen from '../images/productTen.jpg';
 
 const initialState = {
   // State for products
   productCatalogue: [
     {
-      image: '../../images/IMG_0949.jpg.webp',
+      image: productOne,
       title: "Men's Sweater - Sage",
       price: 750.00
     },
     {
-      image: '../../images/IMG_0895.jpg.webp',
+      image: productTwo,
       title: "Unisex Tee - Grey Melange - Plato",
       price: 250.00
     },
     {
-      image: '../../images/IMG_0874.jpg.webp',
+      image: productThree,
       title: "Unisex Tee - Grey Melange - Ō",
       price: 250.00
     },
     {
-      image: '../../images/IMG_0716.jpg.webp',
+      image: productFour,
       title: "Ladies Sweater - Sage",
       price: 750.00
     },
     {
-      image: '../../images/productFive.jpg',
+      image: productFive,
       title: "Unisex Sweater - Grey Melange",
       price: 500.00
     },
     {
-      image: '../../images/productSix.jpg',
+      image: productSix,
       title: "Plato House Blend - 1kg",
       price: 350.00
     },
     {
-      image: '../../images/productSeven.jpg',
+      image: productSeven,
       title: "Plato house blend - 250g",
       price: 100.00
     },
     {
-      image: '../../images/productEight.jpg',
+      image: productEight,
       title: "Plato Socks",
       price: 189.00
     },
     {
-      image: '../../images/productNine.jpg',
+      image: productNine,
       title: "Plato Travel Mug",
       price: 649.00
     },
     {
-      image: '../../images/productTen.jpg',
+      image: productTen,
       title: "Plato Water Bottle",
       price: 799.00
     }
