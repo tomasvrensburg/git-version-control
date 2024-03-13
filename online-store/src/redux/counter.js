b5c7740b4e5f9956df302f425b2322b983c1614f
tomasvrensburg <tomasvrensburg@gmail.com>
2024-03-13 12:55:38 +0200

    Added state objects for products in counter.js

--- online-store/src/redux/counter.js
+++ online-store/src/redux/counter.js
@@ -1,18 +1,69 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+
 const initialState = {
-  value: 0,
-}
+  // State for products
+  productCatalogue: [
+    {
+      image: '../../images/IMG_0949.jpg.webp',
+      title: "Men's Sweater - Sage",
+      price: 750.00
+    },
+    {
+      image: '../../images/IMG_0895.jpg.webp',
+      title: "Unisex Tee - Grey Melange - Plato",
+      price: 250.00
+    },
+    {
+      image: '../../images/IMG_0874.jpg.webp',
+      title: "Unisex Tee - Grey Melange - Ō",
+      price: 250.00
+    },
+    {
+      image: '../../images/IMG_0716.jpg.webp',
+      title: "Ladies Sweater - Sage",
+      price: 750.00
+    },
+    {
+      image: '../../images/productFive.jpg',
+      title: "Unisex Sweater - Grey Melange",
+      price: 500.00
+    },
+    {
+      image: '../../images/productSix.jpg',
+      title: "Plato House Blend - 1kg",
+      price: 350.00
+    },
+    {
+      image: '../../images/productSeven.jpg',
+      title: "Plato house blend - 250g",
+      price: 100.00
+    },
+    {
+      image: '../../images/productEight.jpg',
+      title: "Plato Socks",
+      price: 189.00
+    },
+    {
+      image: '../../images/productNine.jpg',
+      title: "Plato Travel Mug",
+      price: 649.00
+    },
+    {
+      image: '../../images/productTen.jpg',
+      title: "Plato Water Bottle",
+      price: 799.00
+    }
+  ]
+};
+
+
 
 export const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
     increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.value += 1
     },
     decrement: (state) => {
