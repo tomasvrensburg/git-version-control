0dc86fca3f032ec401c8747cfac891063699e2fd
tomasvrensburg <tomasvrensburg@gmail.com>
2024-03-13 08:08:48 +0200

    Removed import for dropdown in Header.js

--- online-store/src/components/Header/Header.js
+++ online-store/src/components/Header/Header.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 // Importing Logo and icons
 import logo from '../../images/platoLogo.webp';
