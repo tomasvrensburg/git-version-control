0b8d3807cef712f737135a3a15b1f2c6a0a2e6c7
tomasvrensburg <tomasvrensburg@gmail.com>
2024-03-14 09:22:32 +0200

    Added shipping methods and info modal to ShoppingCart.js

--- online-store/src/components/ShoppingCart/ShoppingCart.js
+++ online-store/src/components/ShoppingCart/ShoppingCart.js
@@ -1,7 +1,11 @@
+import InfoModal from '../InfoModal';
+import React from 'react';
 // Import Stylesheet
 import './shoppingCart.css';
 // Import Icons
 import { FaPlus, FaMinus } from "react-icons/fa";
+import { IoIosHelpCircleOutline } from "react-icons/io";
+
 // Importing bootstrap components
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -14,13 +18,14 @@ export default function ShoppingCart() {
   const cart = useSelector((state) => state.counter.cart);
   const dispatch = useDispatch();
 
+  // State for shipping info
+  const [modalShow, setModalShow] = React.useState(false);
+
   // Calculating total amount of cart
   let total = 0;
   cart.forEach(item => {
     total += (item.quantity * item.price);
   });
-
-  console.log(total);
 
   return (
     <div className="ShoppingCart">
@@ -55,7 +60,35 @@ export default function ShoppingCart() {
             <hr />
           </>
         ))}
-        <h2>Total: R {total}</h2>
+        <div id='checkout'>
+          <h2>Total: R {total}</h2>
+          <h3>
+            Shipping
+            <IoIosHelpCircleOutline className='icon help-icon' onClick={() => setModalShow(true)} />
+          </h3>
+
+          {/* Modal for shipping info */}
+          <InfoModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+          />
+
+          {/* Input for shipping method */}
+          <div className='block'>
+            <div>
+              <input type='radio' id='express' name='shippingMethod' value='express' />
+              <label htmlFor='standard'>Express Shipping - R150</label>
+            </div>
+            <div>
+              <input type='radio' id='standard' name='shippingMethod' value='standard' />
+              <label htmlFor='standard'>Standard Shipping - R60</label>
+            </div>
+            <div>
+              <input type='radio' id='collect' name='shippingMethod' value='collect' />
+              <label htmlFor='collect'>Collection - R30</label>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
